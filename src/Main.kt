ab99010f1550e20e1398f713ba15796fd63c3abd
Mohamed Madooh <mmadooh14@gmail.com>
2025-02-18 19:41:40 +0300

    done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    println("Hello World!")
+    var ahmed: UserRole = UserRole.ADMIN
+    checkAccess(ahmed)
+    for (entry in UserRole.entries) {
+        println(entry)
+    }
+    println(UserRole.entries)
+}
+
+enum class UserRole(accessLevel: Int) {
+    ADMIN(1), EDITOR(2), VIEWER(3)
+}
+
+fun checkAccess(accessLevel: UserRole) {
+    when (accessLevel) {
+        UserRole.ADMIN -> println("User role is admin")
+        UserRole.EDITOR -> println("User role is editor")
+        UserRole.VIEWER -> println("User role is viewer")
+    }
 }
